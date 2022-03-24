14a2e2311115babd740ee2c13964ae9faaf0d019
Euijin <yuijin0909@gmail.com>
2022-03-24 14:02:10 +0900

    Manifest 수정

--- app/src/main/java/com/example/part5_chapter1/Part5Chapter1Application.kt
+++ app/src/main/java/com/example/part5_chapter1/Part5Chapter1Application.kt
@@ -7,7 +7,7 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-class Part5Chapter01Application: Application() {
+class Part5Chapter1Application: Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -15,7 +15,7 @@ class Part5Chapter01Application: Application() {
 
         startKoin {
             androidLogger(Level.ERROR)
-            androidContext(this@Part5Chapter01Application)
+            androidContext(this@Part5Chapter1Application)
             modules(appModule)
 
 
